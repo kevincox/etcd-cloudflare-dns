041d3ea3801802eaad69be438ed3964c069389a9
Kevin Cox <kevincox@kevincox.ca>
2016-03-06 15:54:13 -0500

    Change to nix-ci marathon config.

--- default.nix
+++ default.nix
@@ -1,7 +1,33 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
+with import <nixpkgs> {}; let
+	marathon = [{
+		id = "/etcd-cloudflare-dns";
+		instances = 1;
+		
+		cpus = "JSON_UNSTRING 0.01 JSON_UNSTRING";
+		mem = 50;
+		disk = 0;
+		
+		cmd = ''
+			set -a
+			source /etc/kevincox-environment
+			source /run/keys/cloudflare
+			nix-store -r PKG --add-root pkg --indirect
+			exec sudo -E -uetcd-cloudflare-dns PKG/bin/etcd-cloudflare-dns
+		'';
+		env = {
+			CF_DOMAIN = "kevincox.ca";
+		};
+		user = "root";
+		
+		upgradeStrategy = {
+			minimumHealthCapacity = 0;
+			maximumOverCapacity = 0;
+		};
+	}];
+in stdenv.mkDerivation {
 	name = "etcd-cloudflare-dns";
+	
+	outputs = ["out" "marathon"];
 	
 	meta = {
 		description = "Keep cloudflare dns records in sync with data in etcd.";
@@ -29,5 +55,12 @@ stdenv.mkDerivation {
 		
 		wrapProgram $out/bin/etcd-cloudflare-dns \
 			--set RUBYLIB "$out/bundle"
+		
+		# Marathon config.
+		install ${builtins.toFile "marathon" (builtins.toJSON marathon)} "$marathon"
+		substituteInPlace "$marathon" \
+			--replace '"JSON_UNSTRING' "" \
+			--replace 'JSON_UNSTRING"' "" \
+			--replace PKG "$out"
 	'';
 }
